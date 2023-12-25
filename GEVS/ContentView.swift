20a37a78c194a48df1561ca69c94a1d7c562b050
b1ms <bimzy@outlook.com>
2023-12-25 14:50:57 +0000

    Initial Commit

---
+++ GEVS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GEVS
+//
+//  Created by Bimsara W Perera on 25/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GEVS/GEVSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GEVSApp.swift
+//  GEVS
+//
+//  Created by Bimsara W Perera on 25/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GEVSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
